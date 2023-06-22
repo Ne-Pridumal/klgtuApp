63a911ce887647957d6eba4ed750074d73f220e4
Ne-Pridumal <ivan.gavrilkov@gmail.com>
2023-06-22 13:02:28 +0200

    fix

---
+++ docs-build/assets/preview-17103ee6.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./iframe-73f9ec13.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-EYKKDMVH-2105096a.js").then(e=>e.D),["./DocsRenderer-EYKKDMVH-2105096a.js","./iframe-73f9ec13.js","./index-c013ead5.js","./_commonjsHelpers-725317a4.js","./_getPrototype-dd8f0158.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-17103ee6.js.map
